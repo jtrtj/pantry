82646ec2d0dcdb0384b408c2a14457d72db5dc80
JohnRoemer <jt.r@live.com>
2018-06-05 09:12:31 -0600

    first two tests passing

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,16 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_starts_with_an_empty_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_you_can_check_the_stock_of_an_item
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check('Cheese')
+  end
 
 end
