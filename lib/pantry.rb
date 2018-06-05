972261f21e0cd662d7520512e083cbe88871c429
JohnRoemer <jt.r@live.com>
2018-06-05 09:53:08 -0600

    can now add recipes to shopping list

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,7 +1,11 @@
+require './lib/recipe.rb'
+
 class Pantry
-  attr_reader :stock
+  attr_reader :stock,
+              :shopping_list
   def initialize
-    @stock = Hash.new(0)
+    @stock         = Hash.new(0)
+    @shopping_list = {}
   end
 
   def stock_check(item)
@@ -10,5 +14,9 @@ class Pantry
 
   def restock(item, quantity)
     stock[item] += quantity
+  end
+
+  def add_to_shopping_list(recipe)
+    @shopping_list = recipe.ingredients.merge(shopping_list)
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,6 +1,7 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class PantryTest < Minitest::Test
   def test_it_starts_with_an_empty_stock
@@ -25,4 +26,14 @@ class PantryTest < Minitest::Test
     assert_equal 30,pantry.stock_check('Cheese')
   end
 
+  def test_you_can_add_recipes_to_the_shopping_list
+    recipe = Recipe.new('Cheese Pizza')
+    recipe.add_ingredient('Cheese', 20)
+    recipe.add_ingredient('Flour', 20)
+
+    pantry = Pantry.new
+    pantry.add_to_shopping_list(recipe)
+
+    assert_equal ({ 'Cheese' => 20, 'Flour' => 20 }), pantry.shopping_list
+  end
 end
